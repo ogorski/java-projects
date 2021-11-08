86be945a964e4e0dc244a58c9e5af5d01430fb1c
Olegio <ogorski23@gmail.com>
2021-11-08 23:56:05 +0200

    update
    added practise 03 and practise 04
    this branch will be deleted after push

--- src/main/java/practise/practise02.java
+++ src/main/java/practise/practise02.java
@@ -1,5 +1,15 @@
 package practise;
 
+// practise with variables and math operations (for example I use plus)
+
 public class practise02 {
-    
+    public static void main(String[] args) {
+        int a;
+        int b;
+
+        a = 2;
+        b = 3;
+
+        System.out.println(a + b);
+    }
 }
---
+++ src/main/java/practise/practise03.java
@@ -0,0 +1,19 @@
+package practise;
+
+// variables practise with int and String
+
+public class practise03 {
+    public static void main(String[] args) {
+        int age;
+        String city;
+        String name;
+
+        name = "Oleh ";
+        age = 21;
+        city = " Kyiv";
+
+        System.out.print(name);
+        System.out.print(age);
+        System.out.print(city);
+    }
+}
